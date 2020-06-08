dbfb76a1483b0461b72ede681d17f6f96d5dc679
Andrew Landis <andrew.landis3@gmail.com>
2020-06-08 18:05:15 -0400

    Connection to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongodb = require('mongodb')
 const bodyParser = require('body-parser')
 const path = require('path')
 
@@ -13,6 +14,24 @@ app.set('view engine', 'ejs')
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
+})
+
+// MongoDB Setup
+const url = 'mongodb+srv://CrimeHawk:BpgWODrXCT8u0pZS@crimehawk-bdw7f.mongodb.net/CrimeHawk?retryWrites=true&w=majority'
+const dbName = 'CrimeHawk'
+const MongoClient = mongodb.MongoClient
+
+MongoClient.connect(url, (error, client) => {
+  if (error) throw error
+  console.log('Connected successfully to MongoDB Atlas server')
+
+  const db = client.db(dbName)
+
+  db.createCollection('CrimeData')
+  db.listCollections().toArray((error, collections) => {
+    if (error) throw error
+    console.log(collections)
+  })
 })
 
 app.listen(port, () => {
