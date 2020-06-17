15085f1e888e4664ceeb091b342d4b34ab4079a5
Andrew Landis <andrew.landis3@gmail.com>
2020-06-17 15:09:30 -0400

    Get and save data at midnight

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongodb = require('mongodb')
 const bodyParser = require('body-parser')
 const path = require('path')
+const schedule = require('node-schedule')
+const fetch = require('node-fetch')
 
 // Express Setup
 const app = express()
@@ -16,31 +18,46 @@ app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
 })
 
-//node schedule
-var schedule = schedule.scheduleJob({ hour: 00, minute: 00 },
-  function () {
-    console.log('Data updated!')
-  })
-  
 // MongoDB Setup
 const url = 'mongodb+srv://CrimeHawk:BpgWODrXCT8u0pZS@crimehawk-bdw7f.mongodb.net/CrimeHawk?retryWrites=true&w=majority'
 const dbName = 'CrimeHawk'
 const MongoClient = mongodb.MongoClient
+const collectionName = 'CrimeData'
+let db
 
 MongoClient.connect(url, (error, client) => {
   if (error) throw error
   console.log('Connected successfully to MongoDB Atlas server')
 
-  const db = client.db(dbName)
+  db = client.db(dbName)
 
-  db.createCollection('CrimeData')
-  db.listCollections().toArray((error, collections) => {
-    if (error) throw error
-    console.log(collections)
-  })
+  db.createCollection(collectionName)
 })
 
 app.listen(port, () => {
   console.log('Server is up!')
 })
 
+// API request for crime data
+const getCrimeData = async () => {
+  const response = await fetch('https://data.baltimorecity.gov/resource/nhwe-7c7x.json')
+  const data = await response.json()
+  return data
+}
+
+// Node schedule
+const getDataAtMidnight = schedule
+getDataAtMidnight.scheduleJob(
+  { hour: 0, minute: 0 },
+  async () => {
+    // Get crime data
+    const data = getCrimeData()
+
+    // Overwrite all data if there
+    if (data) {
+      await db.dropCollection(collectionName)
+      await db.createCollection(collectionName)
+      db.collection(collectionName).insertMany(data)
+    }
+  }
+)
