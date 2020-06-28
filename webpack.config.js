501da60d219378f9e16d0a35dc661c058a821cac
Andrew Landis <andrew.landis3@gmail.com>
2020-06-28 19:34:30 -0400

    Added Sprint 1 comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+// Completed on 6/28/2020 by Andrew Landis
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
@@ -12,11 +13,11 @@ module.exports = {
       {
         test: /\.js$/,
         loader: ['babel-loader', 'eslint-loader'],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
